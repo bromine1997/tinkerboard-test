e6eb76ef1f59251d2e335ab5f775ca036d7a3202
HyunwooKim <37721713+khyw407@users.noreply.github.com>
2024-03-20 18:43:16 +0900

    docs: nodejs 개발용 툴 설치 추가

--- script/utils.sh
+++ script/utils.sh
@@ -3,4 +3,13 @@ apt-get install git
 
 # Install NVM(node version manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+# Install nodejs lts version
+nvm install 20.11.1
+nvm use 20.11.1
 
+# Update NPM(Node package manager) and core development tools
+npm install -g npm@latest
+npm install -g yarn
+npm install -g @nestjs/cli
+npm install -g ts-node
