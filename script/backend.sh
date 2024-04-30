417003a761c50c73d2a9c09634b02cf820b54a19
HyunwooKim <khyw407@gmail.com>
2024-04-30 18:08:42 +0900

    fix(script): backend 배포 스크립트 심볼릭링크 적용

--- script/backend.sh
+++ script/backend.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # 백엔드 배포 스크립트
 
-directory="/home/linaro/deploy/backend"
-package_directory="/home/linaro/deploy/backend/package"
+archive_directory="/home/linaro/deploy/backend/archive"
 
-if [ ! -d "$directory" ]; then
-    mkdir -p "$directory"
-    mkdir -p "$package_directory"
-    echo "Deployment directory created."
+if [ ! -d "$archive_directory" ]; then
+    mkdir -p "$archive_directory"
+    echo "Archive directory created."
 else
-    echo "Deployment directory already exists."
+    echo "Archive directory already exists."
 fi
 
 # GitHub 프로젝트 주소와 압축 파일 이름을 지정 (마음대로 지정 가능)
@@ -19,7 +17,7 @@ timestamp=$(date +"%Y%m%d%H%M%S")
 new_archive_name="tinkerboard-master-$timestamp.tar.gz"
 
 # 다운로드 디렉토리로 이동합니다.
-cd "$directory" || exit
+cd "$archive_directory" || exit
 
 # 이미 파일이 존재하는지 확인하고, 없으면 다운로드합니다.
 if [ ! -f "$archive_name" ]; then
@@ -33,6 +31,10 @@ fi
 mv "$archive_name" "$new_archive_name"
 
 # 압축해제 & 심볼릭링크 설정
+package_directory="/home/linaro/deploy/backend/package"
+mv "$new_archive_name" "$archive_directory"
+
+ln -s "$archive_directory/tinkerboard-master-$timestamp" "$package_directory"
 
 if [ -f "$new_archive_name" ]; then
     tar -zxvf "$new_archive_name" -C "$package_directory" --strip-components=1
@@ -40,5 +42,9 @@ if [ -f "$new_archive_name" ]; then
 fi
 
 # pm2 재실행
-backend_directory="$package_directory/boilerplate/backend" # 임시 path(마음대로 지정 가능)
-cd "$backend_directory" && npm install && npm run build && pm2 delete ecosystem.config.js && pm2 start ecosystem.config.js
+# path는 임시(마음대로 지정 가능)
+cd "$package_directory/boilerplate/backend" 
+  && npm install 
+  && npm run build 
+  && pm2 delete ecosystem.config.js 
+  && pm2 start ecosystem.config.js
