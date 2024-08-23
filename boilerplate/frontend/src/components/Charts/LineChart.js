a7896b3f061afe780bcc2645ddd5b05ca53a49a8
bromine1997 <109931164+bromine1997@users.noreply.github.com>
2024-08-23 15:08:08 +0900

    Update LineChart.js

--- boilerplate/frontend/src/components/Charts/LineChart.js
+++ boilerplate/frontend/src/components/Charts/LineChart.js
@@ -6,103 +6,34 @@ export default {
   mixins: [mixins.reactiveProp],
   data() {
     return {
-      // 임의의 데이터셋을 설정합니다.
-      data1: [
-        { x: 0, y: 10 },
-        { x: 1, y: 20 },
-        { x: 2, y: 30 },
-        { x: 3, y: 40 },
-        { x: 4, y: 50 },
-      ],
-      data2: [
-        { x: 0, y: 15 },
-        { x: 1, y: 25 },
-        { x: 2, y: 35 },
-        { x: 3, y: 45 },
-        { x: 4, y: 55 },
-      ],
-      totalDuration: 5000,  // 임의의 총 지속시간
-      chartConfig: {
-        type: 'line',
-        data: {
-          datasets: [
-            {
-              borderColor: 'red',
-              borderWidth: 1,
-              radius: 0,
-              data: [],  // 나중에 초기화될 데이터
-            },
-            {
-              borderColor: 'blue',
-              borderWidth: 1,
-              radius: 0,
-              data: [],  // 나중에 초기화될 데이터
-            }
-          ]
-        },
-        options: {
-          animation: {
-            x: {
-              type: 'number',
-              easing: 'linear',
-              duration: this.delayBetweenPoints,
-              from: NaN,
-              delay(ctx) {
-                if (ctx.type !== 'data' || ctx.xStarted) {
-                  return 0;
-                }
-                ctx.xStarted = true;
-                return ctx.index * this.delayBetweenPoints;
-              }
-            },
-            y: {
-              type: 'number',
-              easing: 'linear',
-              duration: this.delayBetweenPoints,
-              from: this.previousY,
-              delay(ctx) {
-                if (ctx.type !== 'data' || ctx.yStarted) {
-                  return 0;
-                }
-                ctx.yStarted = true;
-                return ctx.index * this.delayBetweenPoints;
-              }
-            }
-          },
-          interaction: {
-            intersect: false
-          },
-          plugins: {
-            legend: false
-          },
-          scales: {
-            x: {
-              type: 'linear'
-            }
-          }
-        }
-      }
+      chartDataIndex: 0,
+      intervalId: null,
     };
-  },
-  computed: {
-    delayBetweenPoints() {
-      return this.totalDuration / this.data1.length;
-    },
-    previousY() {
-      return (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
-    }
   },
   methods: {
     initializeChart() {
-      // 데이터셋을 임의의 데이터로 초기화합니다.
-      this.chartConfig.data.datasets[0].data = this.data1;
-      this.chartConfig.data.datasets[1].data = this.data2;
-
-      // 차트를 렌더링합니다.
-      this.renderChart(this.chartConfig.data, this.chartConfig.options);
-    }
+      // Chart.js 차트를 초기화
+      this.renderChart(this.chartData, this.extraOptions);
+    },
+    startDrawing() {
+      // 1초에 하나씩 데이터를 추가
+      this.intervalId = setInterval(() => {
+        if (this.chartDataIndex < this.chartData.datasets[0].data.length) {
+          // 데이터를 하나씩 추가
+          this.$data._chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(this.chartData.datasets[0].data[this.chartDataIndex]);
+          });
+          this.$data._chart.update();
+          this.chartDataIndex += 1;
+        } else {
+          // 모든 데이터가 추가되면 반복 종료
+          clearInterval(this.intervalId);
+        }
+      }, 1000);
+    },
   },
   mounted() {
-    this.initializeChart(); // 컴포넌트가 마운트될 때 차트를 초기화합니다.
-  }
+    this.initializeChart();
+  },
 };
+
