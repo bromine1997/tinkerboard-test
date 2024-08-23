57ea833858aba6c886b45f438d940ac95062b364
bromine1997 <109931164+bromine1997@users.noreply.github.com>
2024-08-23 14:47:31 +0900

    Update Dashboard.vue
    line chart test

--- boilerplate/frontend/src/pages/Dashboard.vue
+++ boilerplate/frontend/src/pages/Dashboard.vue
@@ -1,1 +1,31 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-12">
+        <line-chart
+          style="height: 100%"
+          ref="bigChart"
+          chart-id="big-line-chart"
+        >
+        </line-chart>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/Charts/LineChart';
+
+export default {
+  components: {
+    LineChart,
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  margin-top: 20px;
+}
+</style>
 
