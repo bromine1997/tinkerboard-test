01c12fc132222b4a8569b1517c55a828a6cc8b81
bromine1997 <109931164+bromine1997@users.noreply.github.com>
2024-08-23 15:08:49 +0900

    Update Dashboard.vue

--- boilerplate/frontend/src/pages/Dashboard.vue
+++ boilerplate/frontend/src/pages/Dashboard.vue
@@ -2,9 +2,14 @@
   <div>
     <div class="row">
       <div class="col-12">
+        <button @click="startChartDrawing">Start Drawing</button>
         <card type="chart" style="height: 400px; width: 100%">
           <div class="chart-area" style="height: 100%; width: 100%">
-            <line-chart ref="lineChart"></line-chart>
+            <line-chart
+              ref="lineChart"
+              :chart-data="chartData"
+              :extra-options="chartOptions"
+            />
           </div>
         </card>
       </div>
@@ -19,6 +24,38 @@ export default {
   components: {
     LineChart,
   },
+  data() {
+    return {
+      chartData: {
+        datasets: [
+          {
+            label: 'Dataset 1',
+            borderColor: 'red',
+            data: [
+              { x: 0, y: 10 },
+              { x: 1, y: 20 },
+              { x: 2, y: 30 },
+              { x: 3, y: 40 },
+              { x: 4, y: 50 },
+            ],
+          },
+        ],
+      },
+      chartOptions: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    startChartDrawing() {
+      this.$refs.lineChart.startDrawing();
+    },
+  },
 };
 </script>
 
@@ -27,4 +64,3 @@ export default {
   margin-top: 20px;
 }
 </style>
-
