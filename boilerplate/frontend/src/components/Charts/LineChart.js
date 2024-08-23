8ca0d1a71a03df722f0cda3706ebc58741a1634b
bromine1997 <109931164+bromine1997@users.noreply.github.com>
2024-08-23 15:14:00 +0900

    Update LineChart.js

--- boilerplate/frontend/src/components/Charts/LineChart.js
+++ boilerplate/frontend/src/components/Charts/LineChart.js
@@ -8,25 +8,29 @@ export default {
     return {
       chartDataIndex: 0,
       intervalId: null,
+      chartOptions: {
+        scales: {
+          y: {
+            min: 0,  // y축 최소값
+            max: 5, // y축 최대값, 데이터 범위에 맞게 조정하세요
+          },
+        },
+      },
     };
   },
   methods: {
     initializeChart() {
-      // Chart.js 차트를 초기화
-      this.renderChart(this.chartData, this.extraOptions);
+      this.renderChart(this.chartData, this.chartOptions);
     },
     startDrawing() {
-      // 1초에 하나씩 데이터를 추가
       this.intervalId = setInterval(() => {
         if (this.chartDataIndex < this.chartData.datasets[0].data.length) {
-          // 데이터를 하나씩 추가
           this.$data._chart.data.datasets.forEach((dataset) => {
             dataset.data.push(this.chartData.datasets[0].data[this.chartDataIndex]);
           });
           this.$data._chart.update();
           this.chartDataIndex += 1;
         } else {
-          // 모든 데이터가 추가되면 반복 종료
           clearInterval(this.intervalId);
         }
       }, 1000);
