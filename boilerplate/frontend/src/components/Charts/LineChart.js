9cf7b52f4569b056b54dce56320a62e91ef28141
bromine1997 <109931164+bromine1997@users.noreply.github.com>
2024-08-23 14:35:18 +0900

    Update LineChart.js
    line chart 수정

--- boilerplate/frontend/src/components/Charts/LineChart.js
+++ boilerplate/frontend/src/components/Charts/LineChart.js
@@ -4,52 +4,105 @@ export default {
   name: 'line-chart',
   extends: Line,
   mixins: [mixins.reactiveProp],
-  props: {
-    extraOptions: Object,
-    gradientColors: {
-      type: Array,
-      default: () => ['rgba(72,72,176,0.2)', 'rgba(72,72,176,0.0)', 'rgba(119,52,169,0)'],
-      validator: (val) => {
-        return val.length > 2;
-      },
-    },
-    gradientStops: {
-      type: Array,
-      default: () => [1, 0.4, 0],
-      validator: (val) => {
-        return val.length > 2;
-      },
-    },
-  },
   data() {
     return {
-      ctx: null,
+      // 임의의 데이터셋을 설정합니다.
+      data1: [
+        { x: 0, y: 10 },
+        { x: 1, y: 20 },
+        { x: 2, y: 30 },
+        { x: 3, y: 40 },
+        { x: 4, y: 50 },
+      ],
+      data2: [
+        { x: 0, y: 15 },
+        { x: 1, y: 25 },
+        { x: 2, y: 35 },
+        { x: 3, y: 45 },
+        { x: 4, y: 55 },
+      ],
+      totalDuration: 5000,  // 임의의 총 지속시간
+      chartConfig: {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              borderColor: 'red',
+              borderWidth: 1,
+              radius: 0,
+              data: [],  // 나중에 초기화될 데이터
+            },
+            {
+              borderColor: 'blue',
+              borderWidth: 1,
+              radius: 0,
+              data: [],  // 나중에 초기화될 데이터
+            }
+          ]
+        },
+        options: {
+          animation: {
+            x: {
+              type: 'number',
+              easing: 'linear',
+              duration: this.delayBetweenPoints,
+              from: NaN,
+              delay(ctx) {
+                if (ctx.type !== 'data' || ctx.xStarted) {
+                  return 0;
+                }
+                ctx.xStarted = true;
+                return ctx.index * this.delayBetweenPoints;
+              }
+            },
+            y: {
+              type: 'number',
+              easing: 'linear',
+              duration: this.delayBetweenPoints,
+              from: this.previousY,
+              delay(ctx) {
+                if (ctx.type !== 'data' || ctx.yStarted) {
+                  return 0;
+                }
+                ctx.yStarted = true;
+                return ctx.index * this.delayBetweenPoints;
+              }
+            }
+          },
+          interaction: {
+            intersect: false
+          },
+          plugins: {
+            legend: false
+          },
+          scales: {
+            x: {
+              type: 'linear'
+            }
+          }
+        }
+      }
     };
   },
-  methods: {
-    updateGradients(chartData) {
-      if (!chartData) return;
-      const ctx = this.ctx || document.getElementById(this.chartId).getContext('2d');
-      const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-
-      gradientStroke.addColorStop(this.gradientStops[0], this.gradientColors[0]);
-      gradientStroke.addColorStop(this.gradientStops[1], this.gradientColors[1]);
-      gradientStroke.addColorStop(this.gradientStops[2], this.gradientColors[2]);
-      chartData.datasets.forEach((set) => {
-        set.backgroundColor = gradientStroke;
-      });
+  computed: {
+    delayBetweenPoints() {
+      return this.totalDuration / this.data1.length;
     },
+    previousY() {
+      return (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+    }
+  },
+  methods: {
+    initializeChart() {
+      // 데이터셋을 임의의 데이터로 초기화합니다.
+      this.chartConfig.data.datasets[0].data = this.data1;
+      this.chartConfig.data.datasets[1].data = this.data2;
+
+      // 차트를 렌더링합니다.
+      this.renderChart(this.chartConfig.data, this.chartConfig.options);
+    }
   },
   mounted() {
-    this.$watch(
-      'chartData',
-      (newVal, oldVal) => {
-        this.updateGradients(this.chartData);
-        if (!oldVal) {
-          this.renderChart(this.chartData, this.extraOptions);
-        }
-      },
-      { immediate: true }
-    );
-  },
+    this.initializeChart(); // 컴포넌트가 마운트될 때 차트를 초기화합니다.
+  }
 };
