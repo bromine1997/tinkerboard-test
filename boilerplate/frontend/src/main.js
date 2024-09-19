159c9176d2db42acf95247119c54ee7d978f0efe
bomin seo <sbm8011@gmail.com>
2024-09-19 21:15:43 +0900

    "axios 추가"

--- boilerplate/frontend/src/main.js
+++ boilerplate/frontend/src/main.js
@@ -8,13 +8,16 @@ import BlackDashboard from './plugins/blackDashboard';
 import i18n from './i18n';
 import './registerServiceWorker';
 
+import axios from 'axios';
 
 Vue.use(BlackDashboard);
 Vue.use(VueRouter);
 Vue.use(RouterPrefetch);
 
 axios.defaults.baseURL = 'http://localhost:8080'; // 백엔드 서버 주소
-app.config.globalProperties.$http = axios;
+
+// Vue 2에서 전역으로 axios 설정
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
