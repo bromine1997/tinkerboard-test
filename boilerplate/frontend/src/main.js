d63855e7fa0caeadf89399882c7b4f3e18a4a378
bomin seo <sbm8011@gmail.com>
2024-09-19 17:31:32 +0900

    "main.js backend 서버 포트 변경"

--- boilerplate/frontend/src/main.js
+++ boilerplate/frontend/src/main.js
@@ -7,9 +7,15 @@ import router from './router/index';
 import BlackDashboard from './plugins/blackDashboard';
 import i18n from './i18n';
 import './registerServiceWorker';
+
+
 Vue.use(BlackDashboard);
 Vue.use(VueRouter);
 Vue.use(RouterPrefetch);
+
+axios.defaults.baseURL = 'http://localhost:8080'; // 백엔드 서버 주소
+app.config.globalProperties.$http = axios;
+
 new Vue({
   router,
   i18n,
