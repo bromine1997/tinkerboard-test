af9cf1ab6f1daecf7e54a4fc07d145667e0187c0
bomin seo <sbm8011@gmail.com>
2024-09-25 14:48:19 +0900

    "수정"

--- boilerplate/backend/src/auth/auth.module.ts
+++ boilerplate/backend/src/auth/auth.module.ts
@@ -5,17 +5,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthRepository } from './auth.repository'; // AuthRepository import
+import { databaseProviders } from '../database/database.providers'; // MongoDB providers 설정
+import { UserModule } from '../user/user.module'; // UserModule 임포트
+
 
 @Module({
   imports: [
+    UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'SECRET_KEY', // JWT 비밀 키 설정
       signOptions: { expiresIn: '1d' }, // JWT 만료 시간 설정
     }),
+    
     PassportModule.register({ defaultStrategy: 'jwt' }), // Passport 모듈 설정
   ],
   controllers: [AuthController], // AuthController 등록
-  providers: [AuthService, AuthRepository, JwtStrategy], // AuthService, AuthRepository, JwtStrategy 제공
+  providers: [AuthService, AuthRepository, JwtStrategy,...databaseProviders,], // AuthService, AuthRepository, JwtStrategy 제공
   exports: [AuthService], // AuthService를 다른 모듈에서 사용할 수 있게 내보냄
 })
 export class AuthModule {}
