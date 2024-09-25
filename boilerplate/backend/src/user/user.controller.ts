a1e66ee26ed0d8c4a9ad2b630d273e0ef9a04c40
bomin seo <sbm8011@gmail.com>
2024-09-25 21:19:49 +0900

    "user controller 추가"

--- boilerplate/backend/src/user/user.controller.ts
+++ boilerplate/backend/src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Put, Delete, Body } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiOkResponse, ApiParam } from '@nestjs/swagger';  // ApiParam 추가
 import { UserService } from './user.service';
 import { UserDto } from './user.dto';
 import { UpdateUserDto } from './update-user.dto';
@@ -18,18 +18,21 @@ export class UserController {
 
   @ApiOperation({ summary: '특정 사용자 조회' })
   @ApiOkResponse({ type: UserDto })
+  @ApiParam({ name: 'id', description: '사용자 ID', required: true })  // ApiParam 추가
   @Get(':id')
   async getUserById(@Param('id') id: string) {
     return this.userService.findById(id);
   }
 
   @ApiOperation({ summary: '사용자 정보 수정' })
+  @ApiParam({ name: 'id', description: '사용자 ID', required: true })  // ApiParam 추가
   @Put(':id')
   async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(id, updateUserDto);
   }
 
   @ApiOperation({ summary: '사용자 삭제' })
+  @ApiParam({ name: 'id', description: '사용자 ID', required: true })  // ApiParam 추가
   @Delete(':id')
   async deleteUser(@Param('id') id: string) {
     return this.userService.deleteUser(id);
