86bda3b15128d743b0cedb27cec898f11a24356e
bomin seo <sbm8011@gmail.com>
2024-09-25 14:40:32 +0900

    "user module provider 수정 수정.."

--- boilerplate/backend/src/user/user.module.ts
+++ boilerplate/backend/src/user/user.module.ts
@@ -4,12 +4,14 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository'; // UserRepository import
 import { User, UserSchema } from './user.schema';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // Mongoose 설정
   ],
-  providers: [UserService, UserRepository], // UserRepository를 providers에 추가
+  
+  providers: [UserService, UserRepository,DatabaseModule], // UserRepository를 providers에 추가
   controllers: [UserController],
   exports: [UserService, UserRepository], // 다른 모듈에서 사용할 수 있도록 export
 })
