7ace520707a11e5b852e3fe53910384f703653af
bomin seo <sbm8011@gmail.com>
2024-09-25 15:20:24 +0900

    "core module 추가"

--- boilerplate/backend/src/auth/auth.module.ts
+++ boilerplate/backend/src/auth/auth.module.ts
@@ -6,10 +6,13 @@ import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthRepository } from './auth.repository';
 import { UserModule } from '../user/user.module'; // UserModule 임포트
+import { CoreModule } from '../core/core.module'; // CoreModule 임포트 (ContextLogger 제공)
+
 
 @Module({
   imports: [
     UserModule, // UserModule을 통해 사용자 데이터 접근
+    CoreModule, // ContextLogger가 제공되는 모듈 임포트
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'SECRET_KEY',
       signOptions: { expiresIn: '1d' }, // JWT 만료 시간 설정
