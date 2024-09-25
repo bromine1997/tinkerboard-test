777757d69445b17f7339776e56c74924bed01226
bomin seo <sbm8011@gmail.com>
2024-09-25 21:38:13 +0900

    "test"

--- boilerplate/backend/src/user/user.module.ts
+++ boilerplate/backend/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository'; // UserRepository import
 import { databaseProviders } from '../database/database.providers'; // MongoDB providers 설정
+import { CoreModule } from '../core/core.module'; // CoreModule 임포트 (ContextLogger 제공)
 
 @Module({
-  imports: [],
+  imports: [CoreModule],
   providers: [UserService, UserRepository, ...databaseProviders], // MongoDB providers 추가
   controllers: [UserController],
   exports: [UserService, UserRepository, ...databaseProviders], // 다른 모듈에서 사용할 수 있도록 export
