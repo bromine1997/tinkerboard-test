6fdd6d2030f4fd1216b7932af94b9dac548a83a1
bomin seo <sbm8011@gmail.com>
2024-09-25 15:32:18 +0900

    "dto 이름 수정"

--- boilerplate/backend/src/auth/auth.controller.ts
+++ boilerplate/backend/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, UnauthorizedException, UseFilters } from '@nestjs/common';
 import { ApiOperation, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './Create-User.dto'; // CreateUserDto 가져오기
+import { CreateUserDto } from './Create-User.dto' 
 import { ApiExceptionFilter } from '../core/filter/api-exception.filter'; // 예외 필터 추가
 import { LoginDto } from './login.dto'; // LoginDto 가져오기
 
