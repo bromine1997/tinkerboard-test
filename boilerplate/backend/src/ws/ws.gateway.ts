891defce0b0ff23fa23b3e31001ed3d766f5a721
bomin seo <sbm8011@gmail.com>
2024-11-12 17:29:54 +0900

    "tesst"

--- boilerplate/backend/src/ws/ws.gateway.ts
+++ boilerplate/backend/src/ws/ws.gateway.ts
@@ -64,10 +64,10 @@ export class WsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayD
       });
 
       // 센서 데이터 저장
-      await this.sensorDataService.saveSensorData(sensorDataPacketDto);
-      console.log('센서 데이터 저장 성공');
+      //await this.sensorDataService.saveSensorData(sensorDataPacketDto);
+      //console.log('센서 데이터 저장 성공');
     } catch (error) {
-      console.error('센서 데이터 저장 실패:', error);
+     // console.error('센서 데이터 저장 실패:', error);
     }
   }
 
