045c2544ecfcf61d7cb6508ab63c7d7acd4507e2
bomin seo <sbm8011@gmail.com>
2024-11-21 22:53:32 +0900

    "라인 차트 수정"

--- boilerplate/frontend/src/components/Charts/LineChart.js
+++ boilerplate/frontend/src/components/Charts/LineChart.js
@@ -1,26 +1,23 @@
+import { Line } from "vue-chartjs";
+import { Chart, registerables } from "chart.js";
 
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { defineComponent, ref, watch, onMounted } from "vue";
+Chart.register(...registerables);
 
-export default defineComponent({
-  name: "LineChart",
+export default {
+  name: "line-chart",
+  components: {
+    Line,
+  },
   props: {
+    chartId: {
+      type: String,
+      default: "line-chart",
+    },
     chartData: {
       type: Object,
       required: true,
     },
-    extraOptions: {
-      type: Object,
-      default: () => ({}),
-    },
+    extraOptions: Object,
     gradientColors: {
       type: Array,
       default: () => [
@@ -35,66 +32,49 @@ export default defineComponent({
       default: () => [1, 0.4, 0],
       validator: (val) => val.length > 2,
     },
-    chartId: {
-      type: String,
-      default: "line-chart",
+  },
+  data() {
+    return {
+      chartRef: null,
+    };
+  },
+  methods: {
+    updateGradients() {
+      if (!this.chartData) return;
+      const chartInstance = this.chartRef?.chart;
+      const ctx = chartInstance?.ctx;
+      if (!ctx) return;
+
+      const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+      gradientStroke.addColorStop(this.gradientStops[0], this.gradientColors[0]);
+      gradientStroke.addColorStop(this.gradientStops[1], this.gradientColors[1]);
+      gradientStroke.addColorStop(this.gradientStops[2], this.gradientColors[2]);
+
+      this.chartData.datasets.forEach((set) => {
+        set.backgroundColor = gradientStroke;
+      });
     },
   },
-  setup(props) {
-    const chartCanvas = ref(null); // Canvas reference
-    let chartInstance = null; // Chart.js instance
-
-    const createGradient = (ctx) => {
-      const gradient = ctx.createLinearGradient(0, 230, 0, 50);
-      gradient.addColorStop(props.gradientStops[0], props.gradientColors[0]);
-      gradient.addColorStop(props.gradientStops[1], props.gradientColors[1]);
-      gradient.addColorStop(props.gradientStops[2], props.gradientColors[2]);
-      return gradient;
-    };
-
-    const updateGradients = (chartData) => {
-      if (!chartData) return;
-      const ctx = chartCanvas.value.getContext("2d");
-      const gradient = createGradient(ctx);
-      chartData.datasets.forEach((set) => {
-        set.backgroundColor = gradient;
-      });
-    };
-
-    const renderChart = () => {
-      if (chartInstance) {
-        chartInstance.destroy(); // 기존 차트 삭제
-      }
-
-      const ctx = chartCanvas.value.getContext("2d");
-      updateGradients(props.chartData);
-
-      chartInstance = new ChartJS(ctx, {
-        type: "line",
-        data: props.chartData,
-        options: props.extraOptions,
-      });
-    };
-
-    // Watch for chartData changes
-    watch(
-      () => props.chartData,
-      (newData) => {
-        updateGradients(newData);
-        renderChart();
-      },
-      { deep: true }
-    );
-
-    // Initial rendering
-    onMounted(() => {
-      renderChart();
-    });
-
-    return {
-      chartCanvas,
-    };
+  mounted() {
+    this.updateGradients();
   },
-});
-
-
+  watch: {
+    chartData: {
+      handler() {
+        this.updateGradients();
+      },
+      deep: true,
+    },
+  },
+  render(createElement) {
+    return createElement(Line, {
+      ref: "chartRef",
+      props: {
+        chartId: this.chartId,
+        chartData: this.chartData,
+        options: this.extraOptions,
+      },
+    });
+  },
+};
