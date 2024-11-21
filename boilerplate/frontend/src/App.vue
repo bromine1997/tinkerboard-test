86d729cc6d5f545460522f60d56bca72d0c52285
bomin seo <sbm8011@gmail.com>
2024-11-21 12:53:36 +0900

    "app.vue composition api 추가 및 event bus 삭제"

--- boilerplate/frontend/src/App.vue
+++ boilerplate/frontend/src/App.vue
@@ -1,56 +1,82 @@
 <template>
   <div>
     <notifications></notifications>
-    <router-view :key="$route.fullPath" @new-sensor-data="handleNewSensorData"></router-view>
+    <router-view
+      :key="$route.fullPath"
+      @new-sensor-data="handleNewSensorData"
+    ></router-view>
   </div>
 </template>
 
-
-
 <script>
+import { useSensorDataStore } from '@/store/sensorData';
+import { onMounted, onBeforeUnmount, getCurrentInstance, watch } from '@vue/composition-api';
 
-import { EventBus } from "@/eventBus"; // EventBus를 import
 
 export default {
-  methods: {
-    disableRTL() {
-      if (!this.$rtl.isRTL) {
-        this.$rtl.disableRTL();
+  setup() {
+    const sensorDataStore = useSensorDataStore();
+    const vueInstance = getCurrentInstance().proxy;
+
+    const disableRTL = () => {
+      if (!vueInstance.$rtl.isRTL) {
+        vueInstance.$rtl.disableRTL();
       }
-    },
-    toggleNavOpen() {
-      let root = document.getElementsByTagName("html")[0];
-      root.classList.toggle("nav-open");
-    },
-    handleNewSensorData(data) {
-      // WebSocket을 통해 수신한 센서 데이터를 EventBus로 전송
-      console.log("새 센서 데이터 수신:", data);
-      EventBus.$emit("new-sensor-data", data);
-    },
-  },
-  mounted() {
-    this.$watch("$route", this.disableRTL, { immediate: true });
-    this.$watch("$sidebar.showSidebar", this.toggleNavOpen);
+    };
 
-    // WebSocket 연결 설정
-    this.$socket.on("connect", () => {
-      console.log("웹 WebSocket 연결 성공");
+    const toggleNavOpen = () => {
+      let root = document.getElementsByTagName('html')[0];
+      root.classList.toggle('nav-open');
+    };
+
+    const handleNewSensorData = (data) => {
+      console.log('새 센서 데이터 수신:', data);
+      
+    };
+
+    const handleConnect = () => {
+      console.log('웹 WebSocket 연결 성공');
+    };
+
+    const handleDisconnect = () => {
+      console.log('웹 WebSocket 연결 해제');
+    };
+
+    const handleSensorDataUpdate = (data) => {
+      console.log('진짜 테스트 데이터:', data);
+      sensorDataStore.updateSensorData(data);
+    };
+
+    onMounted(() => {
+      // WebSocket 이벤트 리스너 등록
+      vueInstance.$socket.on('connect', handleConnect);
+      vueInstance.$socket.on('disconnect', handleDisconnect);
+      vueInstance.$socket.on('sensor_data_update', handleSensorDataUpdate);
+
+      // 워처 설정
+      watch(
+        () => vueInstance.$route,
+        () => {
+          disableRTL();
+        },
+        { immediate: true }
+      );
+
+      watch(
+        () => vueInstance.$sidebar.showSidebar,
+        () => {
+          toggleNavOpen();
+        }
+      );
     });
 
-    this.$socket.on("disconnect", () => {
-      console.log("웹 WebSocket 연결 해제");
+    onBeforeUnmount(() => {
+      vueInstance.$socket.off('sensor_data_update', handleSensorDataUpdate);
     });
 
-    // 센서 데이터 수신 이벤트
-    this.$socket.on("sensor_data_update", (data) => {
-      this.handleNewSensorData(data); // EventBus로 데이터 전송
-    });
-  },
-  beforeDestroy() {
-    this.$socket.off("sensor_data_update", this.handleNewSensorData);
+    return {
+      handleNewSensorData,
+    };
   },
 };
 </script>
-
-
-<style lang="scss"></style>
