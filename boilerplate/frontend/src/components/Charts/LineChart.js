01af192f15914303ad36b647441a940ef80f9fde
bomin seo <sbm8011@gmail.com>
2024-11-22 11:12:48 +0900

    "라인차트 컴포넌트 수정"

--- boilerplate/frontend/src/components/Charts/LineChart.js
+++ boilerplate/frontend/src/components/Charts/LineChart.js
@@ -1,21 +1,19 @@
-import { Line } from "vue-chartjs";
-import "chart.js/auto";
+import { Line } from 'vue-chartjs'
+import { defineComponent, ref, watch, onMounted } from 'vue'
+import { Chart as ChartJS } from 'chart.js'
 
-export default {
-  name: "line-chart",
-  components: {
-    Line,
-  },
+export default defineComponent({
+  name: 'line-chart',
+  components: { Line },
   props: {
-    chartId: {
-      type: String,
-      default: "line-chart",
-    },
     chartData: {
       type: Object,
-      required: true,
+      required: true
     },
-    extraOptions: Object,
+    extraOptions: {
+      type: Object,
+      default: () => ({})
+    },
     gradientColors: {
       type: Array,
       default: () => [
@@ -23,55 +21,64 @@ export default {
         "rgba(72,72,176,0.0)",
         "rgba(119,52,169,0)",
       ],
-      validator: (val) => val.length > 2,
+      validator: (val) => val.length > 2
     },
     gradientStops: {
       type: Array,
       default: () => [1, 0.4, 0],
-      validator: (val) => val.length > 2,
-    },
+      validator: (val) => val.length > 2
+    }
   },
-  data() {
-    return {
-      chartRef: null,
-    };
-  },
-  methods: {
-    updateGradients() {
-      if (!this.chartData) return;
-      const chartInstance = this.chartRef?.chart;
-      const ctx = chartInstance?.ctx;
-      if (!ctx) return;
+  setup(props) {
+    const chartId = ref('line-chart-' + Math.random().toString(36).substring(2, 9))
+    const ctx = ref(null)
 
-      const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+    const updateGradients = (chartData) => {
+      if (!chartData) return;
+      
+      const context = ctx.value || document.getElementById(chartId.value).getContext('2d')
+      ctx.value = context
+      
+      const gradientStroke = context.createLinearGradient(0, 230, 0, 50)
+      
+      gradientStroke.addColorStop(
+        props.gradientStops[0],
+        props.gradientColors[0]
+      )
+      gradientStroke.addColorStop(
+        props.gradientStops[1],
+        props.gradientColors[1]
+      )
+      gradientStroke.addColorStop(
+        props.gradientStops[2],
+        props.gradientColors[2]
+      )
 
-      gradientStroke.addColorStop(
-        this.gradientStops[0],
-        this.gradientColors[0]
-      );
-      gradientStroke.addColorStop(
-        this.gradientStops[1],
-        this.gradientColors[1]
-      );
-      gradientStroke.addColorStop(
-        this.gradientStops[2],
-        this.gradientColors[2]
-      );
+      chartData.datasets.forEach((set) => {
+        set.backgroundColor = gradientStroke
+      })
+    }
 
-      this.chartData.datasets.forEach((set) => {
-        set.backgroundColor = gradientStroke;
-      });
-    },
-  },
-  mounted() {
-    this.updateGradients();
-  },
-  watch: {
-    chartData: {
-      handler() {
-        this.updateGradients();
+    watch(
+      () => props.chartData,
+      (newVal) => {
+        updateGradients(newVal)
       },
-      deep: true,
-    },
+      { immediate: true }
+    )
+
+    return {
+      chartId,
+      updateGradients
+    }
   },
-};
+  render() {
+    return (
+      <Line
+        id={this.chartId}
+        data={this.chartData}
+        options={this.extraOptions}
+      />
+    )
+  }
+})
