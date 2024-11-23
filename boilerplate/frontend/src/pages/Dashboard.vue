dbf3f109292279ef2d3ec0e0ceeb4365150a4f57
bomin seo <sbm8011@gmail.com>
2024-11-24 00:55:57 +0900

    "대쉬보드 수정"

--- boilerplate/frontend/src/pages/Dashboard.vue
+++ boilerplate/frontend/src/pages/Dashboard.vue
@@ -45,7 +45,7 @@
           <div class="chart-area">
             <v-chart
               :option="mainChartOptions"
-              
+              class="v-chart"
             />
           </div>
         </card>
@@ -227,8 +227,16 @@ export default {
   font-size: 16px;
   color: #FFF;
 }
+.card {
+  height: 100%; /* 카드 컨테이너 높이 */
+}
 .chart-area {
+  width: 100%; /* 카드 내부에 꽉 차도록 설정 */
+  height: 100%; /* 부모 컨테이너 높이에 맞춤 */
   max-height: 500px; /* 최대 높이 설정 (필요 시 변경 가능) */
 }
-
+.v-chart {
+  width: 100%;
+  height: 100%; /* 차트가 부모의 크기에 맞도록 설정 */
+}
 </style>
